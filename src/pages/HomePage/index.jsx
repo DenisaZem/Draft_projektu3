a2a415b21519ae0566473687e380ee20f3f1c7a5
Denisa Zemanova <xdenisa.zemanova@gmail.com>
2024-05-22 11:55:52 +0200

    Zkouška kodu

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,15 @@
+import Dotazník from '../../components/Dotazník';
+import Sounds from '../../components/Sounds';
+import './style.css';
+
+
+export const HomePage = () => {
+  return (
+    <div className="container">
+      <Dotazník />
+      <Sounds />
+     
+      
+    </div>
+  );
+};
